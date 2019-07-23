6f7b98c09123e7464a08027a14e5c928c3d3982e
ekwayv8 <yvon.ekwalla@gmail.com>
2019-07-23 10:47:42 +0200

    Création du dossier complet de notre app

---
+++ app.rb
@@ -0,0 +1,10 @@
+# lignes très pratiques qui appellent les gems du Gemfile. On verra plus tard comment s'en servir ;) - ça évite juste les "require" partout
+require 'bundler'
+Bundler.require
+
+# lignes qui appellent les fichiers lib/user.rb et lib/event.rb
+# comme ça, tu peux faire User.new dans ce fichier d'application. Top.
+require_relative 'lib/user'
+require_relative 'lib/event'
+
+binding.pry
