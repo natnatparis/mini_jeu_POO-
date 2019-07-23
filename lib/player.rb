ea393674aee0b0573ce5320299f9311781edd409
ekwayv8 <yvon.ekwalla@gmail.com>
2019-07-23 11:00:58 +0200

    Création de la méthode initialize

--- lib/player.rb
+++ lib/player.rb
@@ -1,1 +1,20 @@
 require "pry"
+
+class Player
+
+#Définition des attributs d'un player
+attr_accessor :name, :life_points
+
+	#Méthode qui permet de créer nos différents players
+	def initialize(name_str)
+		@name = name_str
+		@life_points = 10
+
+		
+	end
+
+
+
+
+
+end
